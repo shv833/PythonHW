6237643f88c3bc5fbaf9034c0082b87d32cb7f9d
shv833 <volodya.shyra@gmail.com>
2022-04-21 21:48:30 +0300

    Improvement code

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,22 +1,20 @@
-"""дз 182,  323,  560"""
+import random
+
 # 182
-def task182():
-    n = int(input("enter n: "))
-    list = [int(input()) for i in range(0, n)]
+def task_182(n, l):
     amount = 0
     sum = 0
 
     for i in range(0, n):
-        if list[i] % 5 == 0 and not(list[i] % 7 == 0):
+        if l[i] % 5 == 0 and not(l[i] % 7 == 0):
             amount += 1
-            sum += list[i]
-
-    print(amount, sum, sep=", ")
+            sum += l[i]
+    return amount, sum
 
 # 323
-def task323():
-    n = int(input("enter n: "))
-    tmp = n-1
+def task_323(n):
+    tmp = n - 1
+    list = []
     while tmp != 0:
         for i in range(tmp, 0, -1):
             tmp1 = tmp
@@ -27,13 +25,15 @@ def task323():
                 else:
                     tmp1 = tmp1 % tmp2
 
-            if tmp2+tmp1 == 1:
-                print(tmp, i, sep=", ")
+            if tmp2 + tmp1 == 1:
+                list.append(str(tmp) + ' & ' + str(i))
         tmp -= 1
+    return list
 
 # 560
-def task560():
+def task_560():
     f = 0
+    list = []
     for ch in range(200, 301):
         if ch != f:
             sum1 = 0
@@ -45,14 +45,20 @@ def task560():
                 if sum1 % j == 0:
                     sum2 += j
             if sum2 == ch != sum1:
-                print(ch, sum1)
+                list.append(str(ch) + ' ' + str(sum1))
                 f = sum1
-
+    return list
 
 if __name__ == "__main__":
+    n = int(input("enter n: "))
+    random_list = []
+    while len(random_list) != n:
+        random_list.append(random.randint(1, 100))
+
     print("Task182: ")
-    task182()
+    print("Given list ", random_list)
+    print(task_182(n, random_list))
     print("Task323: ")
-    task323()
+    print(task_323(n))
     print("Task560: ")
-    task560()
+    print(task_560())
